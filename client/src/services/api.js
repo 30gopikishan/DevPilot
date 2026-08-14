8338f8c2f22b9b6351c344868dbf830aa99518a0
Gopi Kishan Sahu <gopi_2024bite034@nitsri.ac.in>
2026-08-14 12:43:37 +0530

    Change API base URL to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://devpilot-backend-bxly.onrender.com/api"
 });
 
 api.interceptors.request.use(
